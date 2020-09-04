f669e5b378baf94cec4cd32ddceb16bff39bb75a
kentaroupstream <arc.hang.ski@gmail.com>
2020-09-04 18:23:53 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,8 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index    post = Post.create(content: params[:content], checked: false)
-    render json:{ post: post }
+    post = Post.create(content: params[:content], checked: false)     #最初から既読/未読のchecked属性を「false/未読」で付与するようにした！
+    render json:{ post: post }    #非同期通信(リロードしない)ので、リダイレクトではなくJSON形式で直接データを読み込む方法にした。
   end
 
   def checked
